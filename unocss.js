ffc3e480ddc76a71d4a42d9e3a215f3ae30e6309
4614s <onurgral1446.2@gmail.com>
2025-11-10 21:53:56 +0300

    Add files via upload

---
+++ unocss.js
@@ -0,0 +1,7 @@
+window.__unocss = {
+  shortcuts: {
+    "c-body": "bg-gray-50 dark:bg-gray-700 dark:text-white m-0 selection:bg-emerald-700 selection:text-white font-sans",
+    "c-ip-output": "border-1 border-black border-solid p-3 mb-15 rounded",
+    "c-p": "p-3 pt-4 md:p-5"
+  }
+};
